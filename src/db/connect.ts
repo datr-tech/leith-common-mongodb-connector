c5e038c0d3ff79dba364994d0c527a483e6b5688
joealdersonstrachan <joealdersonstrachan@strachan.email>
2025-04-25 20:05:49 +0100

    feat: add pass and user param options

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,14 +1,21 @@
 import { logger } from '@datr.tech/leith-common-logger';
 import mongoose from 'mongoose';
 
-export const connect = async ({ dbHost, dbName, dbPort }) => {
+export const connect = async ({ host, name, pass, port, user }) => {
   const stat = { isConnected: false, message: undefined };
 
   try {
-    await mongoose.connect(`mongodb://${dbHost}:${dbPort}/${dbName}`);
+    if (pass && user) {
+      await mongoose.connect(
+        `mongodb://${user}:${pass}@${host}:${port}/${name}`,
+      );
+    } else {
+      await mongoose.connect(`mongodb://${host}:${port}/${name}`);
+    }
+
     stat.isConnected = true;
 
-    logger.info(`api has connected to mongodb/${dbName} on port ${dbPort}`);
+    logger.info(`api has connected to mongodb/${name} on port ${port}`);
   } catch (error) {
     const { message } = error;
     stat.message = message;
