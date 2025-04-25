4f56f012751a21595c8865f9d09449ea731c4aa6
joealdersonstrachan <joealdersonstrachan@strachan.email>
2025-04-25 19:42:05 +0100

    feat: add db connect params

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,15 +1,14 @@
 import { logger } from '@datr.tech/leith-common-logger';
 import mongoose from 'mongoose';
 
-export const connect = async () => {
+export const connect = async ({ dbHost, dbName, dbPort }) => {
   const stat = { isConnected: false, message: undefined };
-  const { DB_HOST, DB_NAME, DB_PORT } = process.env;
 
   try {
-    await mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`);
+    await mongoose.connect(`mongodb://${dbHost}:${dbPort}/${dbName}`);
     stat.isConnected = true;
 
-    logger.info(`api has connected to mongodb/${DB_NAME} on port ${DB_PORT}`);
+    logger.info(`api has connected to mongodb/${dbName} on port ${dbPort}`);
   } catch (error) {
     const { message } = error;
     stat.message = message;
